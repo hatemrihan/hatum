051a0ecb1f132dda3111c5b9afa11f34675ce6b0
hatemrihan <your-email@example.com>
2025-06-13 05:53:21 +0300

    for the 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // output: 'export', // Disabled because we have API routes
     // basePath: '/hatum',
     typescript: {
         ignoreBuildErrors: true,
